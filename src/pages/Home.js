bfc1805008f4afabed880d962e83eba67d8242b4
mkathywill <124532909+mkathywill@users.noreply.github.com>
2023-02-14 15:49:46 +0100

    Remove Form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,15 +41,16 @@ function Home() {
       <HowItWorks />
       <Footer />
 
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+//       <form ref={form} onSubmit={sendEmail}>
+//         <label>Name</label>
+//         <input type="text" name="user_name" />
+//         <label>Email</label>
+//         <input type="email" name="user_email" />
+//         <label>Message</label>
+//         <textarea name="message" />
+//         <input type="submit" value="Send" />
+//       </form>
+    
     </>
   );
 }
